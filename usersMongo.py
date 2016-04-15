d70e6fe0fc48beccde3045d18632ab877510c1db
Dhananjay Pandit <dj.pandit007@gmail.com>
2016-04-15 11:42:41 +0530

    User operations using MongoDB
    Create, verify, delete a user. Also contains functionality to record the user-word attempts

---
+++ usersMongo.py
@@ -0,0 +1,64 @@
+from pymongo import MongoClient
+client = MongoClient()
+
+usersDB = client['vocabulary-builder']
+users = usersDB['users']
+
+def createUser(username, name, userType, secret):
+	"""
+	Creates new user if username is not already taken
+	"""
+	if users.posts.find_one({'username' : username}):
+		print "Sorry! This username has already been taken"
+		newUser = None
+	else:
+		user = {'username' : username,
+				'name' : name,
+				'userType' : userType,
+				'secret' : secret
+		}
+		newUser = users.posts.insert_one(user)
+		print "User " + str(username) + " successfully created!"
+
+def deleteUser(username):
+	"""
+	Deletes user if user is present
+	"""
+	if users.posts.find_one({'username' : username}):
+		delUser = users.posts.delete_one({'username' : username})
+		print 'User ' + str(username) + ' successfully deleted!'
+	else:
+		print "No such user!"
+		delUser = None
+
+def verifyUser(username):
+	"""
+	Verifies the user.
+	Prints whether the user exists or not
+	"""
+	if users.posts.find_one({'username' : username}):
+		print "User already exists"
+	else:
+		print "This username is available"
+
+def updateAttempts(username, word, answer):
+	"""
+	Updates the appropriate word dictionary and it's corresponding attributes
+	answer = 1 means correct
+	answer = -1 means wrong
+	"""
+	myID = {'username' : username}
+	totalAttempts = word + '.totalAttempts'
+	correctAttempts = word + '.correctAttempts'
+	wrongAttempts = word + '.wrongAttempts'
+	wordClass = word + '.wordClass'
+	if answer == 1:
+		update = users.posts.update(myID, {'$inc' : {totalAttempts : 1,
+													correctAttempts : 1,
+													wordClass : 1}})
+	elif answer == -1:
+		update = users.posts.update(myID, {'$inc' : {totalAttempts : 1,
+													wrongAttempts : 1,
+													wordClass : -1}})
+	else:
+		print 'Invalid input for parameter : answer'
